7a8bebf7d3d92cbd7dbd0d1f4c3ea7ae7e95fe4f
Matthew Migliore <mmigliore311@gmail.com>
2018-10-19 16:33:33 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -10,6 +10,21 @@ def consolidate_cart(cart)
       groceries[item][:count] = 0
       name = item
       binding.pry
+      
+      array.each do |hash|
+        if hash[:name] == name
+          original_hash[:count] += 1
+        end
+      end
+      
+      cart.each do |item_data|
+        binding.pry
+        if item == name
+          groceries[item][:count] += 1
+        end
+      end
+      
+      
     end 
   end
   
